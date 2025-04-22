56d8d176177425b922d57eafe16924220b4c8562
student-09-429 <student-09-429@STUDENT>
2025-04-22 15:55:14 +0300

    Добавьте файлы проекта.

---
+++ Лаба_4/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string now;
+	int count = 0;
+	string city[10];
+	for (int i = 0; i < 10; ++i) { 
+		cin >> now;
+		city[i] = now;
+		if (now.back() == 'b') {
+			count++;
+		}
+	}
+	cout << "count: " << count;
+	return 0;
+}
